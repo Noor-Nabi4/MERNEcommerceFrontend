100c55e37d2c70ed252a388c36f8511eda975ad7
unknown <gcnoor6@gmail.com>
2023-08-13 01:25:36 -0700

    push

--- src/features/product-list/productSlice.js
+++ src/features/product-list/productSlice.js
@@ -1,21 +1,29 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts } from './productAPI';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchAllProducts, fetchProductsByFilter } from "./productAPI";
 
 const initialState = {
   products: [],
-  status: 'idle',
+  status: "idle",
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
-  'product/fetchAllProducts',
+  "product/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
     return response.data;
   }
 );
 
+export const fetchProductsByFilterAsync = createAsyncThunk(
+  "product/fetchProductsByFilter",
+  async ({filter,sort}) => {
+    const response = await fetchProductsByFilter(filter,sort);
+    return response.data;
+  }
+);
+
 export const productSlice = createSlice({
-  name: 'product',
+  name: "product",
   initialState,
   reducers: {
     increment: (state) => {
@@ -27,17 +35,23 @@ export const productSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchAllProductsAsync.pending, (state) => {
-        state.status = 'loading';
+        state.status = "loading";
       })
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-        state.status = 'idle';
+        state.status = "idle";
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFilterAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductsByFilterAsync.fulfilled, (state, action) => {
+        state.status = "idle";
         state.products = action.payload;
       });
   },
 });
 
-export const { increment} = productSlice.actions;
+export const { increment } = productSlice.actions;
 export const slectAllProducts = (state) => state.product.products;
-
 
 export default productSlice.reducer;
